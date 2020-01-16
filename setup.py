9445f2789c79181afcbe448ce45f1cee57cdb15e
mb-wali <44528277+mb-wali@users.noreply.github.com>
2020-01-16 12:18:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,44 @@ from setuptools import find_packages, setup
 readme = open('README.rst').read()
 
 tests_require = [
-    # 'check-manifest>=0.25',
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'invenio-app>=1.0.4',
+    'invenio-mail>=1.0.0',
+    'invenio-userprofiles>=1.0.0',
+    'isort>=4.3.3',
+    'mock>=2.0.0',
+    'pydocstyle>=2.0.0',
+    'pytest-cov>=2.5.1',
+    'pytest-invenio>=1.1.0',
+    'pytest-pep8>=1.0.6',
+    'redis>=2.10.5',
 
 ]
 
+extras_require = {
+    'docs': [
+        'Sphinx>=1.5.1',
+    ],
+    'mysql': [
+        'invenio-db[mysql]>=1.0.0',
+    ],
+    'postgresql': [
+        'invenio-db[postgresql]>=1.0.0',
+    ],
+     'sqlite': [
+        'invenio-db>=1.0.0',
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+
+for name, reqs in extras_require.items():
+    if name in ('mysql', 'postgresql', 'sqlite'):
+        continue
+    extras_require['all'].extend(reqs)
+    
 setup_requires = [
     'Babel>=1.3',
     'pytest-runner>=3.0.0,<5',
@@ -47,7 +81,8 @@ setup(
     license='MIT',
     author='TUGRAZ',
     author_email='mb_wali@hotmail.com',
-    download_url='https://github.com/mb-wali/invenio-SAML/archive/0.0.1.tar.gz',
+    include_package_data=True,
+    platforms='any',
     entry_points={
         'invenio_base.apps': [
             'invenio_saml = invenio_saml:InvenioSAML',
@@ -59,7 +94,7 @@ setup(
             'invenio_saml = invenio_saml.config',
         ],
     },
-    # extras_require=extras_require,
+    extras_require=extras_require,
     install_requires=install_requires,
     setup_requires=setup_requires,
     tests_require=tests_require,
@@ -68,11 +103,16 @@ setup(
         'Intended Audience :: Developers',
         "License :: OSI Approved :: MIT License",
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 5 - Production/Stable',
     ],
-    python_requires='>=3.6',
+
 
 )
